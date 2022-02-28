4290b7b033b5ba7860eeb26355dfed154c9757ed
Kasun Sampath <kasun.sampath@cloudsolution-sa.com>
2022-02-28 16:38:21 +0530

    memento description added

--- src/com/patternFinder/Main.java
+++ src/com/patternFinder/Main.java
@@ -6,7 +6,12 @@ import com.patternFinder.mementoPattern.History;
 public class Main {
 
     public static void main(String[] args) {
-        /** Memento Pattern **/
+        /** MEMENTO PATTERN **/
+            /** The memento pattern is a software design pattern that provides the
+             * ability to restore an object to its previous state (undo via rollback).
+             * The memento pattern is implemented with three objects: the originator (here Editor),
+             * a caretaker (here History) and a memento (here EditorState).
+             * The originator is some object that has an internal state.**/
         Editor editor = new Editor();
         History history = new History();
 
